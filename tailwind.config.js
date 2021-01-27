3134de933413a4ebdedd03cd2aded171d1798d83
daniel-lesio <neoonepl@gmail.com>
2021-01-27 16:07:34 +0100

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [],
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}','./Layout/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}','./Blocks/**/*.{js,ts,jsx,tsx}','./Layout/**/*.{js,ts,jsx,tsx}'],
 
   darkMode: false, // or 'media' or 'class'
   theme: {
